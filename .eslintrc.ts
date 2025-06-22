054351434217866b6ae76b022e71cb1980e6eac6
Thealireza <imirzaeealireza@gmail.com>
2025-06-22 12:28:00 +0330

    add lint and husky

---
+++ .eslintrc.ts
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', 
+    'prettier', 
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
